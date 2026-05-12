1d31ecbf32afc3163c2eb032f9cf4a0d597fe2cc
Rucha <imt_2025071@iiitm.ac.in>
2026-05-12 16:54:32 +0530

    Initial commit

---
+++ src/components/Icons.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { Switch } from 'react-native';
+
+type IconsProps = PropsWithChildren<{
+    name:string;
+}>
+
+const Icons=({name}) =>{
+    Switch(name){
+        case 'circle':
+          return <Icon name ="circle-thin" size={38} 
+          color="#f7cd2e"/>
+          break;
+        case 'cross':
+          return <Icon name ="times" size={38} 
+          color="#38cc77"/>
+          break;
+        default:
+          return <Icon name ="pencil" size={38} 
+          color="#0d0d0d"/>
+          break;
+    }
+
+}
+
+export default Icons
