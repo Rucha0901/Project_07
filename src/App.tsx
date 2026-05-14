48087edaf1e3e74076b60bc403b01c028d8838cd
Rucha <imt_2025071@iiitm.ac.in>
2026-05-14 20:05:25 +0530

    Fixed Icons component switch cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
+  SafeAreaView,
 } from 'react-native-safe-area-context';
 
+import  {Snackbar}  from 'react-native-snackbar';
+import Icons from './components/Icons';
+import { useState } from 'react';
+
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isCross,setIsCross]=useState<boolean>(false)
+  const [gameWinner,setGameWinner]=useState<string>('')
+  const [gameState,setGameState]=useState(new Array(9).fill('empty',0,9))
+
+  const reloadGame=()=>{
+    setIsCross(false)
+    setGameWinner('')
+    setGameWinner(new Array(9).fill('empty',0,9))
+  }
+
+  const checkIsWinner=()=>{
+    //checking winner of game
+    if(
+      gameState[0]===gameState[1]&&
+      gameState[0]===gameState[2]&&
+      gameState[0]!=='empty'
+    ){
+      setGameWinner(`${gameState[0]} won the game!`);
+    } else if(
+      gameState[4]===gameState[5]&&
+      gameState[3]===gameState[4]&&
+      gameState[3]!=='empty'
+    ){
+      setGameWinner(`${gameState[3]} won the game!`);
+    } else if(
+      gameState[6]===gameState[7]&&
+      gameState[7]===gameState[8]&&
+      gameState[6]!=='empty'
+    ){
+      setGameWinner(`${gameState[6]} won the game!`);
+    } else if(
+      gameState[0]===gameState[3]&&
+      gameState[3]===gameState[6]&&
+      gameState[0]!=='empty'
+    ){
+      setGameWinner(`${gameState[0]} won the game!`);
+    } else if(
+      gameState[4]===gameState[7]&&
+      gameState[1]===gameState[4]&&
+      gameState[1]!=='empty'
+    ){
+      setGameWinner(`${gameState[1]} won the game!`);
+    } else if(
+      gameState[5]===gameState[8]&&
+      gameState[2]===gameState[5]&&
+      gameState[2]!=='empty'
+    ){
+      setGameWinner(`${gameState[2]} won the game!`);
+    } else if(
+      gameState[4]===gameState[8]&&
+      gameState[0]===gameState[4]&&
+      gameState[0]!=='empty'
+    ){
+      setGameWinner(`${gameState[0]} won the game!`);
+    } else if(
+      gameState[4]===gameState[6]&&
+      gameState[2]===gameState[4]&&
+      gameState[2]!=='empty'
+    ){
+      setGameWinner(`${gameState[2]} won the game!`);
+    } else if(!gameState.includes('empty',0)){
+      setGameWinner("Draw game......");
+    }
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+    <SafeAreaView>
+      <StatusBar 
+       />
+       <View>
+        <Text>Tic Tac Toe</Text>
+       </View>
+    </SafeAreaView>
+    
   );
 }
 
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import type { PropsWithChildren } from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import { Switch } from 'react-native';
 
 type IconsProps = PropsWithChildren<{
     name:string;
 }>
 
 const Icons=({name}:IconsProps) =>{
-    Switch(name){
+    switch(name){
         case 'circle':
-          return <Icon name ="circle-thin" size={38} 
-          color="#f7cd2e"/>
-          break;
+          return (<Icon name ="circle-thin" size={38} 
+          color="#f7cd2e"/>)
+          
         case 'cross':
-          return <Icon name ="times" size={38} 
-          color="#38cc77"/>
-          break;
+          return (<Icon name ="times" size={38} 
+          color="#38cc77"/>)
+          
         default:
-          return <Icon name ="pencil" size={38} 
-          color="#0d0d0d"/>
-          break;
+          return (<Icon name ="pencil" size={38} 
+          color="#0d0d0d"/>)
+          
     }
 
 }
